1ecd213c804c4aba5fc3b9ef42f1969e8707175c
Gabriel EUDES <geudes@e3r7p5.clusters.42paris.fr>
2023-01-21 21:38:19 +0100

    NVM NOT DONE AF

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: geudes <geudes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 14:34:49 by geudes            #+#    #+#             */
-/*   Updated: 2023/01/21 03:23:06 by geudes           ###   ########.fr       */
+/*   Updated: 2023/01/21 20:59:44 by geudes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,20 +45,19 @@ int	main(int ac, char **av, char **env)
 	i = 1;
 	while (++i < ac - 1)
 	{
-		pipe(pipes);
+		if (pipe(pipes))
+			(printf("Error"), exit(0));
 		input = pipes[0];
 		output = pipes[1];
 		if (i == 2)
 			input = (close(input), open(av[1], O_RDONLY));
 		if (i == 2 && input == -1)
-			(printf("Input file error"), exit(0));
+			(printf("Input file error\n"), exit(0));
 		if (i == ac - 2)
 			output = (close(output), open(av[ac - 1],
 						O_TRUNC | O_CREAT | O_RDWR, 0000644));
-		if (i < ac - 2)
-			create_that_fork(av[i], env, input, output);
-		else
-			waitpid(create_that_fork(av[i], env, input, output), 0, 0);
+		waitpid(create_that_fork(av[i], env, input, output), 0, 0);
+		printf("Done waiting %s\n", av[i]);
 		close(input);
 		close(output);
 	}
