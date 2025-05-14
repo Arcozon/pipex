eddbbaeb6a69b45de2aff44631dc297b0626a39f
Gabriel Eudes <gaeudes@student.42.fr>
2025-05-14 16:04:19 +0200

    HEREDOC BITCHES

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -6,24 +6,86 @@
 /*   By: gaeudes <gaeudes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 17:57:43 by gaeudes           #+#    #+#             */
-/*   Updated: 2025/05/13 18:14:49 by gaeudes          ###   ########.fr       */
+/*   Updated: 2025/05/14 16:02:10 by gaeudes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+// #include "pipex.h"
 
-void	read_stdin(int fd, char *limiter)
+#include <unistd.h>
+
+// neg error, 0 success, pos not found
+int	read_start_line(int fd, char *limiter, char *c, int *br)
 {
-	size_t	i
+	size_t	i_l;
+
+	i_l = 0;
+	while (1)
+	{
+		*br = read(STDIN_FILENO, c, 1);
+		if (!*br)
+			continue ;
+		if (*br < 0)
+			return (-1);
+		if (!limiter[i_l] && *c == '\n')
+			return (0);
+		if (*c != limiter[i_l])
+			break ;
+		++i_l;
+	}
+	write(fd, limiter, i_l);
+	write(fd, c, 1);
+	return (1);
+}
+
+int	read_stdin(int fd, char *limiter)
+{
+	char	c;
+	int		br;
+
+	c = '\n';
+	br = 1;
+	while (1)
+	{
+		if (br < 0)
+			return (-1);
+		if (c == '\n')
+		{
+			if (!read_start_line(fd, limiter, &c, &br))
+				break ;
+			continue ;
+		}
+		br = read(STDIN_FILENO, &c, 1);
+		if (br == 1)
+			write(fd, &c, 1);
+	}
+	return (0);
 }
 
 int	heredoc(char *limiter)
 {
-	int	doc_p[2];
+	int	here_p[2];
 
-	if (pipe(pipe) < 0)
+	if (pipe(here_p) < 0 || read_stdin(here_p[1], limiter))
 		return (-1);
-	read_stdin(doc_p[1], limiter);
-	close(doc_p[1]);
-	return (doc_p[0]);
+	close(here_p[1]);
+	return (here_p[0]);
+}
+
+int main()
+{
+	int	fd = heredoc("123");
+	
+	if (fd >= 0)
+	{
+		char	txt[512];
+		int		br;
+		do
+		{
+			br = read(fd, txt, 512);
+			if (br < 0)
+				return (write(2, "Merde\n", 6), 1);
+			write(1, txt, br);
+		}	while (br);
+	}
 }
